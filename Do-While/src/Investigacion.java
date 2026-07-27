190b1e8aeef76a17eca6887b1d40e7af5e308c16
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-27 17:06:32 -0500

    feat: realizo la investigacion sobre el Do-While

---
+++ Do-While/src/Investigacion.java
@@ -0,0 +1,17 @@
+public class Investigacion {
+
+    public static void main(String[] args){
+
+        //Investigación: Ciclo do-while en Java
+        //
+        //El ciclo do-while es una estructura repetitiva que ejecuta un bloque de código al menos una vez, ya que primero realiza las instrucciones y después evalúa la condición.
+        //
+        //Su principal diferencia con el ciclo while es que el while primero verifica la condición y luego ejecuta el código, mientras que el do-while primero ejecuta el código y después verifica si debe volver a repetirlo.
+
+        do {
+
+            // Instrucciones
+
+        } while (condicion);
+    }
+}

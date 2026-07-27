faac80054134f6a93a78500cc04232605a092d25
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-27 17:24:50 -0500

    feat: realice ejercicio 1 - contar del 1 al 10

---
+++ Do-While/src/Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args){
+
+        int numero = 1;
+
+        do {
+            System.out.println(numero);
+            numero++;
+        }while(numero <= 10);
+    }
+}

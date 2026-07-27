815aa3844e84a4400db23fed348a4895c2ddc814
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-27 17:44:53 -0500

    feat: realice ejercicio 4 - Mostrar los números pares del 2 al 20

---
+++ Do-While/src/Ejercicio4.java
@@ -0,0 +1,13 @@
+public class Ejercicio4 {
+
+    public static void main(String[] args){
+
+        int numero = 2;
+
+        do {
+            System.out.println(numero);
+
+            numero = numero + 2;
+        }while (numero <= 20);
+    }
+}

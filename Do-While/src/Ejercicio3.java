46bd56ec4d14c0843ea248ab428d349a6fa93f05
Jhon Anderson Rubiano Monroy <ttrolling182@gmail.com>
2026-07-27 17:39:11 -0500

    feat: realice ejercicio 3 - Validar una contraseña

---
+++ Do-While/src/Ejercicio3.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args){
+
+        String clave;
+
+        do {
+            clave = JOptionPane.showInputDialog("Ingrese la contraseña: ");
+        }while (!clave.equals("1234"));
+
+        JOptionPane.showMessageDialog(null , "Contraseña correcta");
+    }
+}
